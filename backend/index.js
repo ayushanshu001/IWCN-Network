ea581ad726b1981f19193586ec5f83754cc043d6
Ayush Anshu <ayushanshu56@gmail.com>
2023-04-12 21:53:46 +0530

    crud updated

--- backend/index.js
+++ backend/index.js
@@ -1,77 +1,93 @@
-const express=require('express');
-require('dotenv').config();
-const Sequelize=require('sequelize');
-const cors=require('cors')
+const express = require("express");
+require("dotenv").config();
+const Sequelize = require("sequelize");
+const cors = require("cors");
 
-const seq=new Sequelize('note_app','root',process.env.PASSWORD,{
-    host:'localhost',
-    dialect:'mysql'
-} );
+const PORT = process.env.PORT;
 
-const note=seq.define('notes',{
-    content:Sequelize.STRING
-})
+const seq = new Sequelize("note_app", "root", process.env.PASSWORD, {
+  host: "localhost",
+  dialect: "mysql",
+});
 
-const app=express();
+const note = seq.define("notes", {
+  content: Sequelize.STRING,
+});
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to Note App')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Note App");
+});
 
-app.post('/notes/posts',async (req,res)=>{
-    const {content}=req.body
-    try{
-      const data= await note.create({
-            content
-        })
-         res.status(200).send(data)
+app.post("/notes/posts", async (req, res) => {
+  const { content } = req.body;
+  try {
+    const data = await note.create({
+      content,
+    });
+    res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
 
-    }catch(err){
-        console.log(err)
-        res.status(400).send({
-            message:err.message
-        })
-    }
-})
+app.get("/notes", async (req, res) => {
+  try {
+    const data = await note.findAll();
+    res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
 
-app.get('/notes',async (req,res)=>{
-    try{
-        const data=await note.findAll()
-        res.status(200).send(data)
-    }catch(err){
-        console.log(err)
-        res.status(400).send({
-            message:err.message
-        })
-    }
-})
+app.put("/notes/edit/:id", async (req, res) => {
+  try {
+    const { content } = req.body;
+    const noteId = req.params.id;
+    const result = await note.update({ content }, { where: { id: noteId } });
+    res.status(200).send({
+      isError: false,
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
 
-app.delete('/notes/:id',async (req,res)=>{
-    try{
-        const data=await note.destroy({
-            where:{
-                id:req.params.id
-            } 
-        })
-        res.status(200).send({
-            isError:false,
-            data
-        })
+app.delete("/notes/delete/:id", async (req, res) => {
+  try {
+    const data = await note.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send({
+      isError: false,
+      data,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
 
-    }catch(err){
-        console.log(err)
-        res.status(400).send({
-            message:err.message
-        })
-    }
-})
-
-
-seq.sync().then(()=>{
-    app.listen(8001,()=>{
-        console.log(`DB connected PORT ${8001}`)
-    })
-})
+seq.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`DB connected PORT ${8001}`);
+  });
+});
--- frontend/src/Components/Note.jsx
+++ frontend/src/Components/Note.jsx
@@ -1,78 +1,104 @@
-import React, { useEffect, useState } from 'react'
-import '../Styles/Note.css'
-import axios from 'axios'
-
+import React, { useEffect, useState } from "react";
+import "../Styles/Note.css";
+import axios from "axios";
 
 const Note = () => {
-   const [text,setText]=useState('')
-   const [data,setData]=useState([])
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
 
-
-   const getData=async ()=>{
-    try{
-        const data=await axios.get(`http://localhost:8001/notes`)
-        setData(data.data)
-        
-    }catch(err){
-        console.log(err)
+  const getData = async () => {
+    try {
+      const data = await axios.get(`http://localhost:8001/notes`);
+      setData(data.data);
+    } catch (err) {
+      console.log(err);
     }
-   }
+  };
 
-   useEffect(()=>{
-    getData()
-   },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
-
-    const handleAdd=async ()=>{
-        const data={content:text}
-      try{
-        const addData=await axios.post(`http://localhost:8001/notes/posts`,data)
-        alert('Note Added')
-        getData()
-        
-
-      }catch(err){
-        console.log(err)
-      }
-      setText('')
-
+  const handleAdd = async () => {
+    const data = { content: text };
+    try {
+      const addData = await axios.post(
+        `http://localhost:8001/notes/posts`,
+        data
+      );
+      alert("Note Added");
+      getData();
+    } catch (err) {
+      console.log(err);
     }
+    setText("");
+  };
 
-
-    const handleDelete=async (id)=>{
-        try{
-            await axios.delete(`http://localhost:8001/notes/${id}`)
-            alert('Note Deleted')
-            getData()
-        }catch(err){
-            console.log(err)
-        }
-
+  const handleEdit = async (id) => {
+    const content = prompt("Enter new content");
+    try {
+      await axios.put(`http://localhost:8001/notes/edit/${id}`, {
+        content,
+      });
+      alert("Note Updated");
+      getData();
+    } catch (err) {
+      console.log(err);
     }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8001/notes/delete${id}`);
+      alert("Note Deleted");
+      getData();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
-    <div className='note'>
-        <textarea id='textArea' placeholder='Write a note here...' value={text} onChange={(e)=>setText(e.target.value)} />
-        <br/>
+      <div className="note">
+        <textarea
+          id="textArea"
+          placeholder="Write a note here..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <br />
         <button onClick={handleAdd}>Add Note</button>
-    </div>
+      </div>
 
-    <div style={{display:'flex',gap:'20px',marginTop:'50px',justifyContent:'center'}}>
-        {
-            data.map((e)=>{
-                return (
-                    <div style={{boxShadow:'rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;'}} key={e.id}>
-                        {e.id}
-                        <p>{e.content}</p>
-                        <button onClick={()=>handleDelete(e.id)}>Delete</button>
-                    </div>
-                )
-            })
-        }
-    </div>
-
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          gap: "20px",
+          marginTop: "50px",
+          justifyContent: "center",
+        }}
+      >
+        {data.map((e) => {
+          return (
+            <>
+              <div
+                style={{
+                  boxShadow:
+                    " rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
+                }}
+                key={e.id}
+              >
+                {e.id}
+                <p>{e.content}</p>
+                <button onClick={() => handleEdit(e.id)}>Edit</button>
+                <button onClick={() => handleDelete(e.id)}>Delete</button>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
